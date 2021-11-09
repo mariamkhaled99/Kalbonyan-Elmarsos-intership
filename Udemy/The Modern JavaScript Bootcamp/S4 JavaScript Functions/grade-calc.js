64c89e18fd06686e131371eb21db54f170136979
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:38:23 +0200

    Update grade-calc.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/grade-calc.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/grade-calc.js
@@ -1,1 +1,31 @@
 The Modern JavaScript Bootcamp
+
+
+let grade_calc= function(student_score,total=100){
+    let percentage=(student_score/total)*100
+    let letter='' // declare here to use it with return 
+    if(percentage>=90){
+        letter='A'
+    }else if(percentage>=80){
+        letter='B'
+    }else if(percentage>=70){
+        letter='C'
+    }else if(percentage>=60){
+        letter='D'
+    }else{
+        letter='f'
+    }
+
+    return `You got a ${letter} (${percentage}%) !`
+}
+
+// tests
+
+let result=grade_calc(80,100)
+console.log(result)
+
+ result=grade_calc(30)
+console.log(result)
+
+ result=grade_calc(98,100)
+console.log(result)
