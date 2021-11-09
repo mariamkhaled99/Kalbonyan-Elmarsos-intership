ab0e731a4186527a9a0f33ae51efd26a8c918f10
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 16:59:26 +0200

    Update expense-tracker.js

--- Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/expense-tracker.js
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/expense-tracker.js
@@ -1,1 +1,40 @@
 The Modern JavaScript Bootcamp
+const account={
+
+    name: 'mariam khaled',
+    expenses: [],
+    income: [],
+    getAccountSummary:function(){
+        //get the total expense
+     let totalExpense =0
+
+     this.expenses.forEach(function(expense){// argument is used to access the expense
+        totalExpense=totalExpense+expense.amount
+     })
+
+     let salary=0
+     this.income.forEach(function(income){
+        salary=salary+income.amount
+    })
+    let accountBalance =0
+    accountBalance =salary-totalExpense
+
+  return `${this.name} has $${totalExpense} in expense her salary is $${salary}  and the account balance is $${accountBalance}`
+    },
+    addExpense:function(description,amount){
+        return this.expenses.push({description,amount})
+
+    },
+    addIncome:function(description,amount){
+        return this.income.push({description,amount})
+
+    },
+
+}
+account.addIncome('driving',2000)
+account.addExpense('Rent',200)
+account.addExpense('Food',300)
+console.log(account.expenses)
+console.log(account.getAccountSummary())
+
+
