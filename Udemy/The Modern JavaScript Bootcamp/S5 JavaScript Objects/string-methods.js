80d2881804c2e3fca3c25fcf568904fa3978f28a
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:48:33 +0200

    Update string-methods.js

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
@@ -1,1 +1,22 @@
 The Modern JavaScript Bootcamp
+
+let isValidPassword=function(pass){
+    /*if((pass.length>=8) && !(pass.includes('password'))){
+        return true
+    }
+    else{
+        return false
+    }*/
+
+    //another way to use if else statments if the return value is true or false .
+    // in this case you should only write your only true condition after return
+
+    return (pass.length>=8) && !(pass.includes('password'))
+}
+
+//suppose to fail because of short length
+console.log(isValidPassword('ghsj'))
+//suppose to success
+console.log(isValidPassword('ghsjhhjjgh'))
+//suppose to fail because of password word
+console.log(isValidPassword('ghsjpassword'))
