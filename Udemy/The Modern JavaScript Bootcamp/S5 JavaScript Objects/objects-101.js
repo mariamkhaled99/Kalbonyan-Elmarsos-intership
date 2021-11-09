04e48426515158aedc8f145ade5af4e2bf649ec1
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:44:57 +0200

    Update objects-101.js

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
@@ -1,1 +1,16 @@
 The Modern JavaScript Bootcamp
+
+
+// person object name ,age , location 
+
+//declare and define the person object 
+
+let person ={
+    name:'mariam',
+    age:26,
+    location:'Tanta'
+}
+
+console.log(`${person.name} is ${person.age} and live in ${person.location}`)
+person.age++
+console.log(`${person.name} is ${person.age} and live in ${person.location}`)
