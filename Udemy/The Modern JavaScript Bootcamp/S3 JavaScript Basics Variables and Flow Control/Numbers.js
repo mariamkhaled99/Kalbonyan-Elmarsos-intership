66e34c2f9b49fc86f4e81df59d234b87c660d41c
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:02:47 +0200

    Update Numbers.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/Numbers.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/Numbers.js
@@ -1,1 +1,13 @@
 The Modern JavaScript Bootcamp
+//<----------------(second challange number)-----------------------
+//------------------calculate the percentage----------------------
+
+// define the variables 
+let studentScore=18
+let maxScore=20
+//calculate the percentage
+let percentage=(studentScore/maxScore)*100
+
+//print the result 
+console.log(percentage)
+
