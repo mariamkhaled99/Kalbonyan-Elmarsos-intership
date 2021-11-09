2efc98b29100e1ce112d68fa7dbf0d603113e247
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:41:41 +0200

    Update number-methods.js

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/number-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/number-methods.js
@@ -1,1 +1,31 @@
 The Modern JavaScript Bootcamp
+
+// guessing game 
+
+
+let guessingNum=function(num){
+
+    //----------------------------Random Formulla------------------------------
+    let min =1
+    let max =5
+   
+    let guessNum= Math.floor(Math.random()*(max-min+1))+min
+
+    //----------------------------another way------------------------------------
+   /* if(num===guessNum){
+         console.log(` the number is ${num} , the guessing number is ${guessNum}`)
+         return true 
+     }
+     else{
+       console.log(` the number is ${num} , the guessing number is ${guessNum}`)
+         return false
+     }*/
+
+
+    return num===guessNum
+}
+
+
+let result= guessingNum(2)
+
+console.log(result)
