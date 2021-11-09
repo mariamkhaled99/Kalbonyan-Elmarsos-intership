d715af67f4f0ab14fcafb4786ad25bd33bd2804b
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:06:18 +0200

    Update if.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/if.js
@@ -1,1 +1,12 @@
 The Modern JavaScript Bootcamp
+//----------------------------------------------------use if statment---------------------
+
+let age=65
+
+if(age<=7){
+    console.log('this is child')
+}
+
+if(age>=65){
+    console.log('this is senior')
+}
