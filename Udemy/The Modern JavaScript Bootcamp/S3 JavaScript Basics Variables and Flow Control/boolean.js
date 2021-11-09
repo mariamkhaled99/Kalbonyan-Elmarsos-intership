c827080b2b19b73caf9a1c4ce4e59cc0a8835cb8
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:03:32 +0200

    Update boolean.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
@@ -1,1 +1,11 @@
 The Modern JavaScript Bootcamp
+//<----------------(third challange boolean)-----------------------
+//------------------calculate the discount for specific set of people----------------------
+
+
+let age=26
+let isChild=age<=7
+let isSenior=age>=65
+
+console.log(isChild)
+console.log(isSenior)
