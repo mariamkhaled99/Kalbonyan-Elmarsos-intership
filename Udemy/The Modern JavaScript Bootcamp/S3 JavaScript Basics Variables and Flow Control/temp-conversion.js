e7a382dc2462560d451814200799f0e0e06b5c7a
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:08:25 +0200

    Update temp-conversion.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,1 +1,12 @@
 The Modern JavaScript Bootcamp
+//<----------------(third challange number)-----------------------
+//------------------convert the temp----------------------
+
+let fahernheit=32
+
+let celius=(fahernheit-32)*(5/9)
+
+let klevin=(fahernheit+459.67)*(5/9)
+
+
+console.log("c=",celius,"k=",klevin)
