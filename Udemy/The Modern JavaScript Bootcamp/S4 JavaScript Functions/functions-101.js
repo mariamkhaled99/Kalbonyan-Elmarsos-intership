861cf6ed1512b8cba0c4048026b01dd164f409a6
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:36:38 +0200

    Update functions-101.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
@@ -1,1 +1,16 @@
 The Modern JavaScript Bootcamp
+
+
+// define function
+let FahernheitTocellusis=function(degree){
+    let celius=(degree-32)*(5/9)
+    return celius
+
+}
+
+// calling function and print the result
+let result=FahernheitTocellusis(32)
+console.log(result)
+
+ result=FahernheitTocellusis(68)
+console.log(result)
