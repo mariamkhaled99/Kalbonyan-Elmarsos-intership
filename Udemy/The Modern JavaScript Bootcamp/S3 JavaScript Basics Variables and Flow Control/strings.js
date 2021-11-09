02aaaa29c858f3e4301b22ed63b3479d0dd455a8
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:07:42 +0200

    Update strings.js

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/strings.js
@@ -1,1 +1,9 @@
 The Modern JavaScript Bootcamp
+//-------------------------first challange--------------------
+//-------------------variable and string concatenation---------
+
+let city='Tanta'
+let country='Egypt'
+let location=city+','+country
+
+console.log(location)
