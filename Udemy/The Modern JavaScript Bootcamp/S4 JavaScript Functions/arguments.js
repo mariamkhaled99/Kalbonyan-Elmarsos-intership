feed6b6a0feaba0d8cdedb6dc5bad5504c433ed9
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2021-11-09 17:37:39 +0200

    Update arguments.js

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/arguments.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/arguments.js
@@ -1,1 +1,24 @@
 The Modern JavaScript Bootcamp
+
+
+let calTips=function(tip_percent=.3,total){
+tip=tip_percent*total
+total+=tip
+
+return "total="+total+" for tip percent ="+tip_percent + " tip value = "+tip
+
+}
+
+// for undefined value
+let result=calTips()
+console.log(result)
+
+// for defined value total 100 ,tips .2
+
+result=calTips(.2,100)
+console.log(result)
+
+// for undefined tip (default value), defined total 100
+
+result=calTips(undefined,100)
+console.log(result)
