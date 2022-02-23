1a89912e4eb2c976b428460d454bb550f1268b60
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2022-02-23 14:55:53 +0200

    Update Debugging-challenge.py
    notes
    debugging is about checking and fixing error 
    
    they are 3 types of error 
    1) logic error .
    2)syntax error
    3) runtime error .
    
    
    test cases :
     commands or scripts designed to test specific scenario

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
@@ -1,1 +1,22 @@
 
+# -------------------------------------error fixing challange--------------------------------------
+
+def plant_recommendation(care):
+    # first error syntax error
+    # if care = 'low':
+    if care == 'low':
+        print('aloe')
+    elif care == 'medium':
+        print('pothos')
+    # second error (logic error)
+    # elif care == 'medium':
+    elif care == 'high':
+        print('orchid')
+
+
+# third error (logic error)
+# plant_rec('low')
+plant_recommendation('low')
+
+plant_recommendation('medium')
+plant_recommendation('high')
