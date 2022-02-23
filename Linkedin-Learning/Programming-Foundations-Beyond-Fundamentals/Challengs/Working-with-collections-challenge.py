ffaaed722c14e5f69bec5cac20e628183d1c3884
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2022-02-23 14:58:20 +0200

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,47 @@
 
+#                         collections and list challange
+
+
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+
+# list of Stars
+
+stars = [
+    'sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+# print the fourth star
+
+print(stars[3])
+
+# ---------------------------------------------------------------------------------------------------------------------------
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+# Collections of Peaks
+
+peak = {
+    'African': 'Kilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua',
+}
+# print the Pacific peak
+
+print(peak['Pacific'])
