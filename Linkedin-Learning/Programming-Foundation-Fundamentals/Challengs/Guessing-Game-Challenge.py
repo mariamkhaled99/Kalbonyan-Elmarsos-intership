2355eb03da04773c2e45b1bf08369f760d405bb3
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2022-02-23 15:03:45 +0200

    Update Guessing-Game-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,12 @@
 
+
+favourite_food = input("whatis your favourite food ?")
+myFavourite_food = "rice and mulokhia"
+
+
+if favourite_food == myFavourite_food:
+    print("Yep, so Amazing !")
+else:
+    print("Yuck,that isn't it !")
+
+print("Thanks , for playing !")
