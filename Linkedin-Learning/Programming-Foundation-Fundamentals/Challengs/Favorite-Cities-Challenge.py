1dae88ad9553f14bb18ca685e520a778c10b792d
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2022-02-23 15:03:09 +0200

    Update Favorite-Cities-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,19 @@
 
+
+def favorite_city(name):
+    return name
+
+
+name = "maka"
+myfavourite_city = favorite_city(name)
+print("one of my favorite city is", myfavourite_city)
+
+name = "alex"
+myfavourite_city = favorite_city(name)
+
+print("one of my favorite city is", myfavourite_city)
+
+name = "indonsia"
+myfavourite_city = favorite_city(name)
+
+print("one of my favorite city is", myfavourite_city)
