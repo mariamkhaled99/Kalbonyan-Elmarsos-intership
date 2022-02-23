249bf90276bcdcee62a99d5ef4171eb2c0a30101
mariamkhaled99 <58251849+mariamkhaled99@users.noreply.github.com>
2022-02-23 14:56:51 +0200

    Update For-loop-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/For-loop-challenge.py
@@ -1,1 +1,18 @@
 
+#                      for loop challange
+
+print('our fruit selection :')
+
+# list of fruits
+
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+# iteration inside the list toprint each fruit
+for fruit in fruits:
+    print(fruit)
